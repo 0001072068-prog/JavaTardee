bef36cb32f9ddefe8ecb3098241fbe8d636a2ece
0001072068-prog <0001072068@senaimgaluno.com.br>
2026-06-10 16:02:33 -0300

    compra online2

---
+++ Compra_online2/Usuario.java
@@ -0,0 +1,54 @@
+package Compra_online2;
+
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String formaPagamento;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+
+    public void exibirDados() {
+
+        System.out.println("Nome: " + getNome());
+        System.out.println("CPF: " + getCpf());
+        System.out.println("Endereco: " + getEndereco());
+        System.out.println("Forma de pagamento: " + getFormaPagamento());
+    }
+}
+
