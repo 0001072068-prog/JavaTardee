4c42f55312f66866020e58029bf92b5e23a61446
0001072068-prog <0001072068@senaimgaluno.com.br>
2026-06-10 16:01:34 -0300

    biblioteca2

---
+++ sistema_biblioteca2/Aluno.java
@@ -0,0 +1,45 @@
+package sistema_biblioteca2;
+
+public class Aluno {
+    private String nome;
+    private int matricula;
+    private boolean possuiMulta;
+    private int livrosEmprestados;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+
+    public boolean isPossuiMulta() {
+        return possuiMulta;
+    }
+
+    public void setPossuiMulta(boolean possuiMulta) {
+        this.possuiMulta = possuiMulta;
+    }
+
+
+    public int getLivrosEmprestados() {
+        return livrosEmprestados;
+    }
+
+    public void setLivrosEmprestados(int livrosEmprestados) {
+        this.livrosEmprestados = livrosEmprestados;
+    }
+}
+
---
+++ sistema_biblioteca2/Livro.java
@@ -0,0 +1,44 @@
+package sistema_biblioteca2;
+
+public class Livro {
+    private String titulo;
+    private int codigo;
+    private boolean disponivel;
+    private int estoque;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
+
